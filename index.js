6243ddc8e9fdc0d17d623ae8fc3774ab460bb13b
gnzlplcs <palaciosgm@outlook.com>
2021-08-18 23:14:58 -0500

    hasta aquí me quedo por ahora

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+
+// escribir un archivo: nombre del archivo, contenido, función
 fs.writeFile('./text.txt', 'text from nodejs', (err) => {
   if (err) {
     console.log(err);
@@ -8,4 +10,18 @@ fs.writeFile('./text.txt', 'text from nodejs', (err) => {
   }
 });
 
+// leer un archivo: nombre del archivo, función
+fs.readFile('./text.txt', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
+})
+
+console.log('esperando a que se ejecute esto');
+console.log('después esto');
+console.log('después esto otro');
+console.log('vamos, ya casi termina');
+console.log('casi la última línea');
 console.log('última línea de código');
